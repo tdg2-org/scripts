2d3bdbf23db441e5d6c7f4087690369c66f442e3
Tony <tonydgoodwin@gmail.com>
2025-01-12 15:43:02 -0500

    WIP. vhdl 2008/2019 for syn_rm.tcl done.  next TODO syn.tcl, imp.tcl

--- tcl/syn_rm.tcl
+++ tcl/syn_rm.tcl
@@ -8,11 +8,31 @@
 # Format for $RMs:  RM0 {RM0_A.sv RM0_B.sv RM0_C.sv} RM1 {RM1_A.sv RM1_B.sv} ... etc.
 #                   RM0 {led_cnt_A.sv led_cnt_B.sv led_cnt_C.sv} RM1 {led_cnt2_A.sv led_cnt2_B.sv led_cnt2_C.sv} RM2 {led_cnt3_A.sv led_cnt3_HH.sv} RM4 {axil_reg32_A.v axil_reg32_B.v}
 
+#--------------------------------------------------------------------------------------------------
+# procs
+#--------------------------------------------------------------------------------------------------
+proc readHDL {fname} {
+  set fType [file extension $fname]
+  if {$fType eq ".v" || $fType eq ".sv"} {
+    read_verilog $fname
+  } elseif {[string match "2008/*" $fname]} {
+    read_vhdl -library work -vhdl2008 $fname
+  } elseif {[string match "2019/*" $fname]} {
+    read_vhdl -library work -vhdl2019 $fname
+  } else {
+    read_vhdl -library work $fname
+  }
+}
+
+#--------------------------------------------------------------------------------------------------
+# main script 
+#--------------------------------------------------------------------------------------------------
+
 
 set hdlDir      [lindex $argv 0]
 set partNum     [lindex $argv 1]
 set RMs         [lindex $argv 2]
-set rmDir       [lindex $argv 3]  ;# output products dir
+set outputDir   [lindex $argv 3]  ;# output products dir
 set RPs         [lindex $argv 4]  ;# module name
 set RPlen       [lindex $argv 5]
 set RMmodName   [lindex $argv 6]
@@ -21,32 +41,33 @@ set RMdir       [lindex $argv 8]
 
 set_part $partNum
 
-# files common to RMs and static in common folder
-set     commonFilesVerilog      [glob -nocomplain -tails -directory $hdlDir/common *.v]
-append  commonFilesVerilog " "  [glob -nocomplain -tails -directory $hdlDir/common *.sv]
+set     commonFilesHDL      [glob -nocomplain -tails -directory $hdlDir/common *.v]
+append  commonFilesHDL  " " [glob -nocomplain -tails -directory $hdlDir/common *.sv]
+append  commonFilesHDL  " " [glob -nocomplain -tails -directory $hdlDir/common *.vhd]
+foreach x $commonFilesHDL {
+  readHDL  $hdlDir/common/$x
+}
 
-foreach x $commonFilesVerilog {
-  read_verilog  $hdlDir/common/$x
+set  commonFilesHDL2008   [glob -nocomplain -tails -directory $hdlDir/common/2008 *.vhd]
+foreach x $commonFilesHDL2008 {
+  readHDL  $hdlDir/common/2008/$x
+}
+
+set  commonFilesHDL2019   [glob -nocomplain -tails -directory $hdlDir/common/2019 *.vhd]
+foreach x $commonFilesHDL2019 {
+  readHDL  $hdlDir/common/2019/$x
 }
 
 # DFX partial only
+# RMmodName will contain 2008/2019 folder for vhdl as part of the filename, if it exists
 if {$RMmodName != ""} {
-  set RMfType [file extension $RMfname]
-  if {$RMfType eq ".v" || $RMfType eq ".sv"} {
-    read_verilog $hdlDir/$RMdir/$RMfname
-  } elseif {[string match "2008/*" $RMfname]} {
-    read_vhdl -library "work" -vhdl2008 "$hdlDir/$RMdir/$RMfname"
-  } elseif {[string match "2019/*" $RMfname]} {
-    read_vhdl -library "work" -vhdl2019 "$hdlDir/$RMdir/$RMfname"
-  } else {
-    read_vhdl -library "work" "$hdlDir/$RMdir/$RMfname"
-  }
+  readHDL $hdlDir/$RMdir/$RMfname
   synth_design -mode out_of_context -top $RMmodName -part $partNum
-  write_checkpoint -force $rmDir/dcp/$RMdir/$RMdir\_post_synth_[file rootname $RMfname].dcp
+  write_checkpoint -force $outputDir/dcp/$RMdir/$RMdir\_post_synth_[file rootname $RMfname].dcp
   return ;# done, return from this script
 }
 
-
+# RMs will contain 2008/2019 folder for vhdl as part of the filename, if it exists
 # loop through every RM per RP, and synthesize all
 for {set idx 0} {$idx <$RPlen} {incr idx} {
   set curRPdir  [lindex $RPs [expr 2*$idx]]
@@ -54,9 +75,12 @@ for {set idx 0} {$idx <$RPlen} {incr idx} {
   set curRMs    [lindex $RMs [expr 2*$idx + 1]]
   puts "\n*** Running $curRPdir, RP module $curRPmod, with RMs: $curRMs ***\n"
   foreach x $curRMs {
-    read_verilog $hdlDir/$curRPdir/$x
+    readHDL $hdlDir/$curRPdir/$x
     synth_design -mode out_of_context -top $curRPmod -part $partNum
-    write_checkpoint -force $rmDir/dcp/$curRPdir/$curRPdir\_post_synth_[file rootname $x].dcp
+    set fileRootName [file rootname $x]
+    if {[string match "2008/*" $fileRootName]} {set fileRootName [string trimleft $fileRootName "2008/"]}
+    if {[string match "2019/*" $fileRootName]} {set fileRootName [string trimleft $fileRootName "2019/"]}
+    write_checkpoint -force $outputDir/dcp/$curRPdir/$curRPdir\_post_synth_$fileRootName.dcp
   }
 }
 
