c6861211345b1ea86d5c8caac0cdc40bd994a22e
Tony <tonydgoodwin@gmail.com>
2025-03-04 11:42:35 -0500

    automated commit & push

--- tcl/syn.tcl
+++ tcl/syn.tcl
@@ -58,6 +58,8 @@ addHDLdir $hdlDir/common
 
 # add submodule hdl directories here
 #addHDLdir ../sub/crc_gen/hdl
+addHDLdir ../sub/common/hdl
+addHDLdir ../sub/common/hdl/bd
 
 # constraints
 set filesXDC [glob -nocomplain -tails -directory $xdcDir *.xdc]
